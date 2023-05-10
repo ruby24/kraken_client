757e6f3acaf7e3d8298b4d0ab34984c3ef376dd6
ruby24 <dolf.diederichsen@gmail.com>
2023-05-10 20:43:08 +0200

    Added withdraw_info method to private.rb
    Added Get Withdrawal Information from https://docs.kraken.com/rest/#tag/User-Funding/operation/getStatusRecentDeposits

--- lib/kraken_client/endpoints/private.rb
+++ lib/kraken_client/endpoints/private.rb
@@ -30,7 +30,8 @@ module KrakenClient
           :TradeVolume   => :trade_volume,
           :AddOrder      => [:add_order,     params: [:pair, :type, :ordertype, :volume]],
           :CancelOrder   => [:cancel_order,  params: [:txid]],
-          :Withdraw      => [:withdraw,      params: [:asset, :key, :amount]]
+          :WithdrawInfo  => [:withdraw_info, params: [:asset, :key, :amount]]
+          :Withdraw      => [:withdraw,      params: [:asset, :key, :amount]],
         }
       end
 
